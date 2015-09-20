8aad170e89b9df998df89ffd356a1a23c15d6fd9
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-09-19 23:03:17 -0300

    Beautify JSON from STDIN if no arguments are passed

--- pybeautifyjson.fish
+++ pybeautifyjson.fish
@@ -1,7 +1,18 @@
 function pybeautifyjson -d "Beautify a JSON string"
   if [ (count $argv) -gt 0 ]
+    switch $argv[1]
+    case "-h*" "--h*" "help"
+      echo "Usage:"
+      echo "  pybeautifyjson '{\"foo\": \"lorem\", \"bar\": \"ipsum\"}'"
+      echo "  echo '{\"foo\": \"lorem\", \"bar\": \"ipsum\"}' | pybeautifyjson"
+      return 0
+    end
     echo $argv | _python -mjson.tool
   else
-    echo "Usage: pybeautifyjson '{\"foo\": \"lorem\", \"bar\": \"ipsum\"}'"
+    set -l input ""
+    while read -z -p "echo ''" -l line
+        set input $input $line
+    end
+    echo $input | _python -mjson.tool
   end
 end
