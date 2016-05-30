dd2ff4f48340661ee4ba54a336c401f41c1591fa
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-05-29 21:19:49 -0300

    pyhttp: simplify port number argument handling

--- functions/pyhttp.fish
+++ functions/pyhttp.fish
@@ -1,9 +1,7 @@
-function pyhttp -d "Start SimpleHTTPServer, optional argument for port number"
-  if test -n "$argv"
-    set HTTPPORT $argv
-  else
-    set HTTPPORT 1025
+function pyhttp -a port -d "Start SimpleHTTPServer, optional argument for port number"
+  if test -z "$port"
+    set port 1025
   end
 
-  __python -m SimpleHTTPServer $HTTPPORT;
+  __python -m SimpleHTTPServer $port;
 end
